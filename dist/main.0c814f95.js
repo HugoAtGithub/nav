aa3b16564ee04f1fd0526a7b0a518f5af3f34b53
HugoAtGithub <jorcion@gmail.com>
2020-10-16 10:24:21 +0800

    first commit

---
+++ dist/main.0c814f95.js
@@ -0,0 +1,209 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"epB2":[function(require,module,exports) {
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+var $siteList = $(".siteList");
+var $lastLi = $siteList.find("li.last");
+var x = localStorage.getItem("x");
+var xObject = JSON.parse(x);
+var dataMap = xObject || [{
+  logo: "A",
+  url: "https://www.acfun.cn"
+}, {
+  logo: "B",
+  url: "https://www.bilibili.com"
+}];
+
+var simplifyUrl = function simplifyUrl(url) {
+  var re = /^(http(s)?:\/\/)?(www\.)?([-a-zA-Z0-9\.]*)/;
+  var result = re.exec(url);
+  return result[4]; // 提取域名，除www以外的 host，即 ([-a-zA-Z0-9\.]*)
+};
+
+var saveData = function saveData() {
+  /* 保存数据 */
+  var dataStr = JSON.stringify(dataMap);
+  localStorage.setItem("x", dataStr);
+};
+
+var render = function render() {
+  /* 重新加载书签 */
+  $siteList.find("li:not(.last)").remove();
+  dataMap.forEach(function (node, index) {
+    var $li = $("\n        <li>\n          <div class=\"site\">\n            <div class=\"logo\">".concat(node.logo, "</div>\n            <div class=\"link\">").concat(simplifyUrl(node.url), "</div>\n            <div class=\"close\">\n              <svg class=\"icon\">\n                <use xlink:href=\"#icon-remove\"></use>\n              </svg>\n            </div>\n          </div>\n        </li>\n      ")).insertBefore($lastLi);
+    $li.on("click", function () {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", function (e) {
+      e.stopPropagation(); // 阻止冒泡
+
+      dataMap.splice(index, 1);
+      render();
+    });
+  }); // 每次重加载都保存一下数据
+
+  saveData();
+}; // 初始化加载
+
+
+render(); // 添加网站事件
+
+$(".addButton").on("click", function () {
+  var url = window.prompt("请输入要添加的网址：");
+
+  if (url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+
+  console.log(url);
+  dataMap.push({
+    logo: simplifyUrl(url)[0].toUpperCase(),
+    url: url
+  });
+  render();
+});
+$(document).on("keypress", function (e) {
+  var key = e.key;
+
+  var _iterator = _createForOfIteratorHelper(dataMap),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var data = _step.value;
+
+      if (data.logo.toLowerCase() === key) {
+        console.log("quick open: ".concat(data.url));
+        window.open(data.url);
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+});
+},{}]},{},["epB2"], null)
+//# sourceMappingURL=main.0c814f95.js.map
---
+++ src/main.js
@@ -0,0 +1,74 @@
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const dataMap = xObject || [
+  { logo: "A", url: "https://www.acfun.cn" },
+  { logo: "B", url: "https://www.bilibili.com" },
+];
+const simplifyUrl = (url) => {
+  const re = /^(http(s)?:\/\/)?(www\.)?([-a-zA-Z0-9\.]*)/;
+  let result = re.exec(url);
+  return result[4]; // 提取域名，除www以外的 host，即 ([-a-zA-Z0-9\.]*)
+};
+
+const saveData = () => {
+  /* 保存数据 */
+  const dataStr = JSON.stringify(dataMap);
+  localStorage.setItem("x", dataStr);
+};
+
+const render = () => {
+  /* 重新加载书签 */
+  $siteList.find("li:not(.last)").remove();
+  dataMap.forEach((node, index) => {
+    const $li = $(`
+        <li>
+          <div class="site">
+            <div class="logo">${node.logo}</div>
+            <div class="link">${simplifyUrl(node.url)}</div>
+            <div class="close">
+              <svg class="icon">
+                <use xlink:href="#icon-remove"></use>
+              </svg>
+            </div>
+          </div>
+        </li>
+      `).insertBefore($lastLi);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation(); // 阻止冒泡
+      dataMap.splice(index, 1);
+      render();
+    });
+  });
+  // 每次重加载都保存一下数据
+  saveData();
+};
+// 初始化加载
+render();
+// 添加网站事件
+$(".addButton").on("click", () => {
+  let url = window.prompt("请输入要添加的网址：");
+  if (url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+  console.log(url);
+  dataMap.push({
+    logo: simplifyUrl(url)[0].toUpperCase(),
+    url: url,
+  });
+  render();
+});
+
+$(document).on("keypress", (e) => {
+  const { key } = e;
+  for (const data of dataMap) {
+    if (data.logo.toLowerCase() === key) {
+      console.log(`quick open: ${data.url}`);
+      window.open(data.url);
+    }
+  }
+});
